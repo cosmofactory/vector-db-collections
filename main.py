fea41fb31294944325f163cffd41b71a56fd4f50
Nikita Assorov <nikssor@yandex.ru>
2023-08-07 21:45:39 +0300

    query issues

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-if __name__ == '__main__':
-    pass
+import chromadb
+
+client = chromadb.PersistentClient(path='./db/')
+
+print(client.list_collections())
+# # print(client.get_collection('umbrella_chats'))
+
+collection = client.get_collection('umbrella_chats')
+# messages = collection.get(
+#     limit=10,
+#     include=['documents', 'metadatas']
+#     )
+
+
+print(collection.query(
+    query_texts='бота',
+    where={'date': '2023-06-01T00:00:00'},
+    include=['documents', 'metadatas']
+))
